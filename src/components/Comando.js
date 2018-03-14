15c814af7f9c851d634184cea6d2802d69440bdc
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-03-13 22:48:17 -0300

    inclusão de button no component Comando
    inclusão de button no component Comando

--- src/components/Comando.js
+++ src/components/Comando.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button } from 'react-native';
 
 export default props => (
-    <Text>Comando</Text>
+    <Button title="Calcular" />
 );
