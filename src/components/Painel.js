3e82f2f7a2ac86a4d409d5ee644d60787e88fb3d
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-03-14 21:43:16 -0300

    utilização do bind para resolver problema de contexto da função
    bind para amarrar para que a função calcular se mantenha dentro do contexto do painel e não no contexto do componente Comando que é de onde está vindo o clique no botão

--- src/components/Painel.js
+++ src/components/Painel.js
@@ -10,10 +10,15 @@ class Painel extends Component {
         super(props);
 
         this.state = { num1: '10', num2: '25' };
+
+        //para resolver estrutura léxica
+        //amarrando para que a função calcular se mantenha dentro do contexto do painel
+        //e não no contexto do componente Comando que é de onde está vindo o clique no botão
+        this.calcular = this.calcular.bind(this);
     }
 
     calcular() {
-
+        const resultado = parseFloat(this.state.num1) + parseFloat(this.state.num2);
     }
 
     render() {
